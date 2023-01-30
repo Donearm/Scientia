b1794c796f28390ee6b037b7d7dd5e311dc62c1d
Gianluca Fiore <gianlucafiore@fountainpencreator.com>
2023-01-30 15:59:28 +0100

    re-enabled search and added search page

---
+++ docs/index.search.js
@@ -0,0 +1,30 @@
+var relearn_search_index = [
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/Personal-Wiki/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "python",
+    "uri": "/Personal-Wiki/tags/python/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Search",
+    "uri": "/Personal-Wiki/search/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/Personal-Wiki/tags/index.html"
+  }
+]
